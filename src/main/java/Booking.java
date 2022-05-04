f4cfa9ba0175b0e55de8d3602f771b583043691d
Bianca Kendall <[D)>
2022-05-04 17:25:43 +0200

    started adding scanners

--- src/main/java/Booking.java
+++ src/main/java/Booking.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Booking {
     private List<Flight> flights;
+    private Scanner scanner;
 
     public Booking() {
         this.flights = new ArrayList<>();
+        this.scanner = new Scanner(System.in);
     }
 
     public void addFlight(Flight flight){
@@ -20,11 +23,23 @@ public class Booking {
         flights.remove(flight);
     }
 
-//    public void addNewPassenger(){
-//
-//    }
+    public void addNewPassenger(){
+        System.out.println("Provide a passenger name: ");
+        String passengerName = scanner.nextLine();
+        System.out.println("Provide a phone number: ");
+        int phoneNumber = scanner.nextInt();
+        System.out.println("Provide a booking ID number: ");
+        int idNumber = scanner.nextInt();
+
+        Passenger passenger = new Passenger(passengerName, phoneNumber, idNumber);
+    }
 
     public void chooseFlight(Passenger passenger, Flight flight) {
+        System.out.println("Which flight (destination) would you like to choose? ");
+        String dest = scanner.nextLine();
+        Flight flightDest = flights
+                .stream()
+                .filter(fl -> fl.getDestination().equals(dest)).map();
         flight.addPassengers(passenger);
         passenger.setFlightId(flight);
 
